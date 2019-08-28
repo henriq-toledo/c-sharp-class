af8130e4bcc52b6176ad9127988d70780d33c4d9
Henrique Toledo <henriq_toledo@yahoo.com.br>
2019-08-28 17:46:18 -0300

    ClassNumber05_OOP: Adding code about OOP on the Employee class.

--- src/CSharpClass/ClassNumber05_OOP/Classes/Employee.cs
+++ src/CSharpClass/ClassNumber05_OOP/Classes/Employee.cs
@@ -2,11 +2,27 @@ namespace ClassNumber05_OOP.Classes
 {
     public class Employee : Person
     {
-        public string test;
+        public string CompanyId { get; private set; }
+        public string RegistrationId { get; private set; }
+        public bool IsTemporary { get; private set; }
 
-        public Employee(string firstName, string lastName, short age)
+        public Employee(string firstName, string lastName, short age, string companyId, string registrationId)
             : base(firstName: firstName, lastName: lastName, age: age)
         {
+            CompanyId = companyId;
+            RegistrationId = registrationId;
+        }
+
+        public Employee(string firstName, string lastName, short age, string companyId)
+            : this(firstName: firstName, lastName: lastName, age: age,
+            companyId: companyId, registrationId: System.Guid.NewGuid().ToString())
+        {
+            this.IsTemporary = true;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Company Id: {CompanyId}, Registration Id: {RegistrationId}, Is Temporary: {IsTemporary}";
         }
     }
 }
--- src/CSharpClass/ClassNumber05_OOP/Program.cs
+++ src/CSharpClass/ClassNumber05_OOP/Program.cs
@@ -7,11 +7,35 @@ namespace ClassNumber05_OOP
     {
         static void Main(string[] args)
         {
-            var person = new Employee(firstName: "João", lastName: "Silva", age: 10);            
-            Console.WriteLine(person.ToString());
+            Person maria = new Person(firstName: "Maria", lastName: "Silva", 19);
 
-            Person joao = new Employee(firstName: "João", lastName: "Silva", age: 10);          
+            Console.WriteLine($"Maria is a person: {(maria is Person)}");
+            Console.WriteLine($"Maria is an employee: {(maria is Employee)}");
+
+            //Employee mariaEmployee = (Employee) maria; // throw exception
+            Employee mariaEmployee = maria as Employee; // null
+
+            Person joao = new Employee(firstName: "João", lastName: "Silva", age: 20, companyId: "Star Company", registrationId: "M2030");          
             Console.WriteLine(joao.ToString());
+
+            Console.WriteLine($"Joao is a person: {(joao is Person)}");
+            Console.WriteLine($"Joao is an employee: {(joao is Employee)}");
+           
+            Employee joaoEmployee = (Employee) joao;
+
+            Employee joseEmployee = new Employee(firstName: "Jose", lastName: "Silva", age: 30, companyId: "Star Company", registrationId: "J1980");
+            Person josePerson = joseEmployee;
+            var companyId = joseEmployee.CompanyId;
+
+            var employee = new Employee(firstName: "Tony", lastName: "Silva", age: 35, 
+                companyId: "Prime Company", registrationId: "T5098");
+
+            Console.WriteLine(employee.ToString());
+
+            var temporaryEmployee = new Employee(firstName: "Pedrinho", lastName: "Silva", age: 21, companyId: "Start Company");
+
+            Console.WriteLine(temporaryEmployee.ToString());
+            
 
             Console.ReadKey();
         }

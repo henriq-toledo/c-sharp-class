a8e442fbfcee34546af58f9d63baaa58eabdbffd
Henrique Toledo <henriq_toledo@yahoo.com.br>
2019-10-09 16:06:52 -0300

    ClassNumber07_Collections: Adding the code about the collections.

---
+++ src/CSharpClass/ClassNumber07_Collections/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ClassNumber07_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
